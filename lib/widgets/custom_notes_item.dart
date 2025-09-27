7646d35f4dc39522c7f86623457b3565601bb37e
youssefkh288 <youkhaled288@gmail.com>
2025-09-27 17:13:50 +0300

    create edit note view

--- lib/widgets/custom_notes_item.dart
+++ lib/widgets/custom_notes_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key, required this.bgColor});
@@ -6,34 +7,46 @@ class NotesItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(color: bgColor, borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
-              child: const Text(
-                'Build your career with khara',
-                style: TextStyle(color: Color.fromARGB(255, 46, 46, 46), fontSize: 16),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(color: bgColor, borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                child: const Text(
+                  'Build your career with khara',
+                  style: TextStyle(color: Color.fromARGB(255, 46, 46, 46), fontSize: 16),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete, color: Colors.black),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.delete, color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Text('August 26, 2004', style: TextStyle(color: Colors.grey[800])),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text('August 26, 2004', style: TextStyle(color: Colors.grey[800])),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/edit_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditViewBody extends StatelessWidget {
+  const EditViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
